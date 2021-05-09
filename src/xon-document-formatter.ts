18d8dffd78e29c520d44b0736d3c6b803d1a5ec3
nizami <nizami.dev@gmail.com>
2021-05-09 23:13:39 +0300

    Update formatting

--- src/xon-document-formatter.ts
+++ src/xon-document-formatter.ts
@@ -1,26 +1,25 @@
-import { CodeFormatter } from '@xon/formatter';
+import { formatCode } from '@xon/formatter';
 import {
-    // CancellationToken,
-    DocumentFormattingEditProvider,
-    // FormattingOptions,
-    ProviderResult,
-    Range,
-    TextDocument,
-    TextEdit,
+  // CancellationToken,
+  DocumentFormattingEditProvider,
+  // FormattingOptions,
+  ProviderResult,
+  Range,
+  TextDocument,
+  TextEdit,
 } from 'vscode';
 
 export class XonDocumentFormatter implements DocumentFormattingEditProvider {
-    provideDocumentFormattingEdits(
-        document: TextDocument
-        // options: FormattingOptions,
-        // token: CancellationToken
-    ): ProviderResult<TextEdit[]> {
-        const firstLine = document.lineAt(0);
-        const lastLine = document.lineAt(document.lineCount - 1);
-        const range = new Range(firstLine.range.start, lastLine.range.end);
+  provideDocumentFormattingEdits(
+    document: TextDocument
+    // options: FormattingOptions,
+    // token: CancellationToken
+  ): ProviderResult<TextEdit[]> {
+    const firstLine = document.lineAt(0);
+    const lastLine = document.lineAt(document.lineCount - 1);
+    const range = new Range(firstLine.range.start, lastLine.range.end);
 
-        const text = document.getText(range);
-        const fmt = new CodeFormatter(text);
-        return [TextEdit.replace(range, fmt.formattedCode())];
-    }
+    const text = document.getText(range);
+    return [TextEdit.replace(range, formatCode(text))];
+  }
 }
