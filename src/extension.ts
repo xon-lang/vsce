8cf00f67a62d6406af5314059edb7a9d9c5796f0
Nizami Amirov <n.amirov@mobigility.com>
2021-06-21 19:57:59 +0300

    Add ts file generation near a xon file

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,18 @@
-import { ExtensionContext, languages } from 'vscode';
+import { clearScopes } from '@xon/ast';
+import { translateModule } from '@xon/translator-ts';
+import * as fs from 'fs';
+import * as path from 'path';
+import {
+  ExtensionContext,
+  languages,
+  TextDocument,
+  window,
+  workspace,
+} from 'vscode';
 import { XonDocumentFormatter } from './xon-document-formatter';
+
+let outputChannel = window.createOutputChannel('XON');
+outputChannel.show();
 
 export function activate(context: ExtensionContext) {
   context.subscriptions.push(
@@ -8,4 +21,24 @@ export function activate(context: ExtensionContext) {
       new XonDocumentFormatter()
     )
   );
+
+  context.subscriptions.push(
+    workspace.onDidSaveTextDocument(onDidSaveTextDocument)
+  );
+}
+
+async function onDidSaveTextDocument(document: TextDocument) {
+  if (document.languageId !== 'xon' || document.uri.scheme !== 'file') return;
+
+  const dirname = path.dirname(document.fileName);
+  const filename = path.parse(document.fileName).name + '.ts';
+  const filePath = path.resolve(dirname, filename);
+
+  clearScopes();
+  try {
+    const translatedXonCode = translateModule(document.getText());
+    fs.writeFileSync(filePath, translatedXonCode);
+  } catch (error) {
+    outputChannel.appendLine(error.toString());
+  }
 }
